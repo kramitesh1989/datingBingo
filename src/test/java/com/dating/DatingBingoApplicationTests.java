adf449b958c3c69b35995355134bb4e8f110dfd9
pg-ta <pg-ta@altimetrik.com>
2022-11-28 14:33:37 +0530

    initial comment

---
+++ src/test/java/com/dating/DatingBingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingBingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
