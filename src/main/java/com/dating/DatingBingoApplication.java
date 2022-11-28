aaeb8f89c2357cf207639743878db05bd930f0c4
pg-ta <pg-ta@altimetrik.com>
2022-11-28 14:41:37 +0530

    add logs

--- src/main/java/com/dating/DatingBingoApplication.java
+++ src/main/java/com/dating/DatingBingoApplication.java
@@ -1,13 +1,17 @@
 package com.dating;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DatingBingoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatingBingoApplication.class, args);
+
+		log.info("############  Server is ready now !! ");
 	}
 
 }
